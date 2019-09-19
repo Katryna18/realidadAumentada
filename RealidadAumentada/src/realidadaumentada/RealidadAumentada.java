b36f076ea8b04ac0b0dae24bfce222baee83f589
USUARIO <USUARIO@PC2>
2019-09-19 09:42:29 -0500

    Creamos el proceso

--- RealidadAumentada/src/realidadaumentada/RealidadAumentada.java
+++ RealidadAumentada/src/realidadaumentada/RealidadAumentada.java
@@ -6,7 +6,14 @@ public class RealidadAumentada {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         
+        int resultado;
+        int numeroUno = 4;
+        int numeroDos =5;
         
+        resultado=numeroDos+numeroUno;
+        
+        System.out.println("El resultado es " + resultado);
+                
         
     }
     
