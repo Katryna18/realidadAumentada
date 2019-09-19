2761924fb26a52d3ae8b43bd1123366324793702
USUARIO <USUARIO@PC2>
2019-09-19 09:39:08 -0500

    Creo Primer Mensaje

--- RealidadAumentada/src/realidadaumentada/RealidadAumentada.java
+++ RealidadAumentada/src/realidadaumentada/RealidadAumentada.java
@@ -4,8 +4,7 @@ package realidadaumentada;
 public class RealidadAumentada {
 
     public static void main(String[] args) {
-        
-        
+        System.out.println("Hola mundo");
         
         
         
